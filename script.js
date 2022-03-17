a52eaa6ed279f32ca50d4745c16684e0df7b2d12
Rishabh Kumar Maurya <62655452+rishabh-maurya@users.noreply.github.com>
2022-03-17 23:53:56 +0530

    fixed bugs
    changes in styles
    adding day, date, month and year

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const time = document.getElementById("time");
+const showHours = document.getElementById("hours");
+const showMinutes = document.getElementById("minutes");
+const showSeconds = document.getElementById("seconds");
 const ampm = document.getElementById("ampm");
+const showDay = document.getElementById("showDay");
+const showDate = document.getElementById("showDate");
+const showMonth = document.getElementById("showMonth");
+const showYear = document.getElementById("showYear");
 
-let abrevationAmPm = (hours) => {
+// weekday and months
+const weekday = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+const months =["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+let abbreviationAmPm = (hours) => {
     if(hours >= 12) {
         return "pm";
     }
@@ -10,40 +20,39 @@ let abrevationAmPm = (hours) => {
     }
 }
 
-// let showTime = () => {
-//     const date = new Date();
-//     const hours = date.getHours();
-//     const minutes = date.getMinutes();
-//     const seconds = date.getSeconds();
-//     const abrevation = abrevationAmPm(hours);
-
-//     time.innerText = `${hours} : ${minutes} : ${seconds}`;
-//     ampm.innerText = `${abrevation}`;
-// }
-
+let isLessThan10 = (value) => {
+    if(value < 10) {
+        return "0" + value;
+    }
+    else {
+        return value;
+    }
+}
 
 setInterval(() => {
     const date = new Date();
     let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    let abrevation = abrevationAmPm(hours);
+    let minutes = isLessThan10(date.getMinutes());
+    let seconds = isLessThan10(date.getSeconds());
+    let abbreviation = abbreviationAmPm(hours);
+    let day = weekday[date.getDay()-1];
+    let datee = isLessThan10(date.getDate());
+    let month = months[date.getMonth()];
+    let year = date.getFullYear();
 
     if(hours >= 12) {
         hours -= 12;
-        hours = "0" + hours;
+        hours = isLessThan10(hours);
     }
 
-    if(minutes < 10) {
-        minutes = "0" + minutes;
-    }
-
-    if(seconds < 10) {
-        seconds = "0" + seconds;
-    }
-
-    time.innerText = `${hours} : ${minutes} : ${seconds}`;
-    ampm.innerText = `${abrevation}`;
+    showHours.innerText = `${hours} :`;
+    showMinutes.innerText = `${minutes} :`;
+    showSeconds.innerText = `${seconds}`;
+    ampm.innerText = `${abbreviation}`;
+    showDay.innerText = `${day}`;
+    showDate.innerText = `${datee}`;
+    showMonth.innerText = `${month}`;
+    showYear.innerText = `${year}`;
 }, 1000);
 
 
